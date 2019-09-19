68c9f71e3bc6aa27f8ee779dc8d96e0d78f8d4f0
cody271 <cody271@gmx.com>
2019-09-18 19:38:10 -0700

    Fix toggle defaults on datasource settings tab

--- src/config_ctrl.ts
+++ src/config_ctrl.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export class IrondbConfigCtrl {
   static templateUrl = 'partials/config.html';
   current: any;
@@ -5,7 +7,7 @@ export class IrondbConfigCtrl {
   constructor($scope) {
     this.current.jsonData.irondbType = this.current.jsonData.irondbType || 'standalone';
     this.current.jsonData.resultsLimit = this.current.jsonData.resultsLimit || '100';
-    this.current.jsonData.useCaching = this.current.jsonData.useCaching || true;
-    this.current.jsonData.activityTracking = this.current.jsonData.activityTracking || true;
+    this.current.jsonData.useCaching = _.defaultTo(this.current.jsonData.useCaching, true);
+    this.current.jsonData.activityTracking = _.defaultTo(this.current.jsonData.activityTracking, true);
   }
 }
