b915d78ae1321248b9b13daa687e3e2e4e85a905
cody271 <cody271@gmx.com>
2019-09-20 15:44:11 -0700

    Add class annotation to IrondbConfigCtrl for Angular injection

--- src/config_ctrl.ts
+++ src/config_ctrl.ts
@@ -4,6 +4,7 @@ export class IrondbConfigCtrl {
   static templateUrl = 'partials/config.html';
   current: any;
 
+  /** @ngInject */
   constructor($scope) {
     this.current.jsonData.irondbType = this.current.jsonData.irondbType || 'standalone';
     this.current.jsonData.resultsLimit = this.current.jsonData.resultsLimit || '100';
