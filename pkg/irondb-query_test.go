1087878cbb3b84e26960cf2bc2be80c41dc63b85
Dave Gray <yargevad+github@gmail.com>
2021-08-12 23:34:44 -0600

    support parsing & translating metric-only "basic" queries to caql

---
+++ pkg/irondb-query_test.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+
+	"github.com/pkg/errors"
+)
+
+func TestDecodeTag(t *testing.T) {
+	for idx, test := range []struct {
+		in  string
+		err error
+		out []byte
+	}{
+		{"", nil, []byte("")},
+		{"foo", nil, []byte("foo")},
+		{"b!%!", errors.New("illegal base64 data at input byte 0"), []byte("")},
+		{"b\"c29tZSBkYXRhIHdpdGggACBhbmQg77u/\"", nil, []byte("some data with \x00 and \ufeff")},
+		{"b!c29tZSBkYXRhIHdpdGggACBhbmQg77u/!", nil, []byte("some data with \x00 and \ufeff")},
+	} {
+		actual, err := decodeTag(test.in)
+		if err == nil && test.err != nil || err != nil && test.err == nil {
+			t.Errorf("DecodeTag[%d] err %q want %q", idx, err, test.err)
+		} else if err != nil && err.Error() != test.err.Error() {
+			t.Errorf("DecodeTag[%d] err %q want %q", idx, err, test.err)
+		} else if bytes.Compare(actual, test.out) != 0 {
+			t.Errorf("DecodeTag[%d] want [%s] got [%s]", idx, test.out, actual)
+		}
+	}
+}
+
+func TestParse(t *testing.T) {
+	for idx, test := range []struct {
+		in  string
+		err error
+		out []Segment
+	}{
+		{"and(__name:duration)", nil, []Segment{Segment{Value: "duration"}}},
+		{"and(__name:duration,foo:bar)", nil, []Segment{
+			Segment{Value: "duration"},
+			Segment{Type: TagCat, Value: "foo"},
+			Segment{Type: TagPair},
+			Segment{Type: TagVal, Value: "bar"},
+			Segment{Type: TagPlus},
+			Segment{Type: TagEnd},
+		}},
+	} {
+		iq := &IrondbQuery{Basic: test.in}
+		err := iq.ParseBasic()
+		if err == nil && test.err != nil || err != nil && test.err == nil {
+			t.Errorf("Parse[%d] err %q want %q", idx, err, test.err)
+		} else if err != nil && err.Error() != test.err.Error() {
+			t.Errorf("Parse[%d] err %q want %q", idx, err, test.err)
+		} else if !reflect.DeepEqual(iq.Segments, test.out) {
+			t.Errorf("Parse[%d] want [%q] got [%q]", idx, test.out, iq.Segments)
+		}
+	}
+}
+
+func TestToCaql(t *testing.T) {
+	for idx, test := range []struct {
+		in   IrondbQuery
+		err1 error
+		err2 error
+		out  string
+	}{
+		{IrondbQuery{Basic: "and(__name:duration)"}, nil, nil, "find('duration')"},
+	} {
+		err := test.in.ParseBasic()
+		if err == nil && test.err1 != nil || err != nil && test.err1 == nil {
+			t.Errorf("Parse[%d] err1 %q want %q", idx, err, test.err1)
+		} else if err != nil && err.Error() != test.err1.Error() {
+			t.Errorf("Parse[%d] err1 %q want %q", idx, err, test.err1)
+		}
+
+		caql, err := test.in.ToCaql()
+		if err == nil && test.err2 != nil || err != nil && test.err2 == nil {
+			t.Errorf("Parse[%d] err2 %q want %q", idx, err, test.err2)
+		} else if err != nil && err.Error() != test.err2.Error() {
+			t.Errorf("Parse[%d] err2 %q want %q", idx, err, test.err2)
+		} else if test.out != caql {
+			t.Errorf("Parse[%d] want [%q] got [%q]", idx, test.out, caql)
+		}
+	}
+}
