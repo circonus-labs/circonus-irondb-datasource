c7a9bceaff546ca48121eef18545362b52533bbc
maier <maier@users.noreply.github.com>
2021-09-10 16:37:20 -0400

    add val param

--- pkg/irondb-backend.go
+++ pkg/irondb-backend.go
@@ -158,7 +158,7 @@ func (td *SampleDatasource) caqlApi(ctx context.Context, q backend.DataQuery, qu
 		RawQuery: qp.Encode(),
 	}
 
-	log.DefaultLogger.Info("caql api query", path.String())
+	log.DefaultLogger.Info("caql api", "path", path.String())
 
 	jsonBytes, err := td.circ.Get(path.String())
 	if err != nil {
