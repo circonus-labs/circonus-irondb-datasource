fb88010102d68860c590cc97e0c38a76a229cf2a
Braden Still-Routley <62965348+R2DToo@users.noreply.github.com>
2026-01-09 14:18:09 -0600

    Updated Grafana dependencies and resolved differences (#139)
    * Updated dependencies and resolved differences
    
    * Use Yarn instead of NPM

--- src/common.ts
+++ src/common.ts
@@ -1,19 +1,8 @@
 import memoize from 'memoizee';
 import { DataSource } from './datasource';
 import _ from 'lodash';
-import {
-  ArrayVector,
-  FieldType,
-  MutableField,
-  TIME_SERIES_TIME_FIELD_NAME,
-  TIME_SERIES_VALUE_FIELD_NAME
-} from '@grafana/data';
-import {
-  CirconusDataSourceOptions,
-  CirconusQuery,
-  SegmentType,
-  TagSet
-} from './types';
+import { FieldType, MutableField, TIME_SERIES_TIME_FIELD_NAME, TIME_SERIES_VALUE_FIELD_NAME } from '@grafana/data';
+import { CirconusDataSourceOptions, CirconusQuery, SegmentType, TagSet } from './types';
 
 export const DEFAULT_QUERY: Partial<CirconusQuery> = {
   queryType: 'graphite',
@@ -68,11 +57,11 @@ export const DURATION_REGEXP = /^([0-9]+)(ms|s|m|h|d)?$/;
 export const DURATION_UNITS_DEFAULT = 's';
 
 export const DURATION_UNITS = {
-    ms: 1,
-    s: 1000,
-    m: 1000 * 60,
-    h: 1000 * 60 * 60,
-    d: 1000 * 60 * 60 * 24,
+  ms: 1,
+  s: 1000,
+  m: 1000 * 60,
+  h: 1000 * 60 * 60,
+  d: 1000 * 60 * 60 * 24,
 };
 
 /**
@@ -82,7 +71,7 @@ export const DURATION_UNITS = {
 export function parseDurationMS(duration: string): number {
   const matches = duration.toLocaleLowerCase().match(DURATION_REGEXP);
   if (!matches) {
-      throw new Error('Invalid time duration: ' + duration);
+    throw new Error('Invalid time duration: ' + duration);
   }
   const units = (matches[2] || DURATION_UNITS_DEFAULT) as keyof typeof DURATION_UNITS;
   return parseInt(matches[1], 10) * DURATION_UNITS[units];
@@ -292,8 +281,7 @@ export function decodeTag(tag: string): string {
   if (/^b(["!]{1}).+\1$/.test(tag)) {
     try {
       tag = atob(tag.slice(2, -1));
-    }
-    catch (e) {}
+    } catch (e) {}
   }
   return tag;
 }
@@ -316,10 +304,10 @@ export function decodeTagsInLabel(label: string): string {
  * counter metric.
  */
 export function isStatsdCounter(name: string): boolean {
-    const rawTags = taglessNameAndTags(name)[1];
-    const tagSet = splitTags(rawTags);
+  const rawTags = taglessNameAndTags(name)[1];
+  const tagSet = splitTags(rawTags);
 
-    return tagSet['statsd_type']?.includes('count');
+  return tagSet['statsd_type']?.includes('count');
 }
 
 /**
@@ -327,7 +315,7 @@ export function isStatsdCounter(name: string): boolean {
  * base64 encoded regular expression tag match (in metric tag queries).
  */
 export function escapeStringForRegExp(regexpString = '') {
-    return String(regexpString).replace(/[\\^$*+?.()|[\]{}]/g, '\\$&');
+  return String(regexpString).replace(/[\\^$*+?.()|[\]{}]/g, '\\$&');
 }
 
 /**
@@ -339,22 +327,22 @@ export function prepStringForRegExp(regexpString = ''): string {
   let fixedRegExp = [];
 
   for (let i = 0; i < regexpString.length; ++i) {
-      const c = regexpString.charAt(i);
-      switch (c) {
-          case '?':
-              fixedRegExp.push('.');
-              break;
+    const c = regexpString.charAt(i);
+    switch (c) {
+      case '?':
+        fixedRegExp.push('.');
+        break;
 
-          case '*':
-              fixedRegExp.push('.*?');
-              break;
+      case '*':
+        fixedRegExp.push('.*?');
+        break;
 
-          default:
-              if (specialChars.indexOf(c) >= 0) {
-                  fixedRegExp.push('\\');
-              }
-              fixedRegExp.push(c);
-      }
+      default:
+        if (specialChars.indexOf(c) >= 0) {
+          fixedRegExp.push('\\');
+        }
+        fixedRegExp.push(c);
+    }
   }
 
   return fixedRegExp.join('');
@@ -385,7 +373,7 @@ export function getTimeField(name: string = TIME_SERIES_TIME_FIELD_NAME): Mutabl
     name: name,
     type: FieldType.time,
     config: {},
-    values: new ArrayVector<number>(),
+    values: [] as number[],
   };
 }
 
@@ -397,7 +385,7 @@ export function getNumberField(name: string = TIME_SERIES_VALUE_FIELD_NAME): Mut
     name: name,
     type: FieldType.number,
     config: {},
-    values: new ArrayVector<number>(),
+    values: [] as string[],
   };
 }
 
@@ -409,7 +397,7 @@ export function getTextField(name: string): MutableField {
     name: name,
     type: FieldType.string,
     config: {},
-    values: new ArrayVector<string>(),
+    values: [] as string[],
   };
 }
 
@@ -421,7 +409,7 @@ export function getOtherField(name: string): MutableField {
     name: name,
     type: FieldType.other,
     config: {},
-    values: new ArrayVector(),
+    values: [],
   };
 }
 
@@ -462,11 +450,9 @@ export function setupCache(useCaching = false, backendSrv: any, datasourceInstan
     const httpMethod = requestOptions.method;
     if (httpMethod === 'GET') {
       return _stripActivityWindow(requestOptions.url);
-    }
-    else if (httpMethod === 'POST') {
+    } else if (httpMethod === 'POST') {
       return JSON.stringify(requestOptions.data);
-    }
-    else {
+    } else {
       throw new Error(`Unsupported HTTP method type: ${httpMethod || '?'}`);
     }
 
@@ -489,148 +475,140 @@ export const md5 = (function (global) {
 
   /* Some IEs are the only ones I know of that need the alternate function */
   if (md5('hello') !== '5d41402abc4b2a76b9719d911017c592') {
-      use_alternate_add32 = true;
+    use_alternate_add32 = true;
   }
 
   return md5;
 
-
   function md5(s: any) {
-      return hex(md51(s));
+    return hex(md51(s));
   }
-
 
   function md5cycle(x: any, k: any) {
-      let a = x[0],
-          b = x[1],
-          c = x[2],
-          d = x[3];
+    let a = x[0],
+      b = x[1],
+      c = x[2],
+      d = x[3];
 
-      a = ff(a, b, c, d, k[0], 7, -680876936);
-      d = ff(d, a, b, c, k[1], 12, -389564586);
-      c = ff(c, d, a, b, k[2], 17, 606105819);
-      b = ff(b, c, d, a, k[3], 22, -1044525330);
-      a = ff(a, b, c, d, k[4], 7, -176418897);
-      d = ff(d, a, b, c, k[5], 12, 1200080426);
-      c = ff(c, d, a, b, k[6], 17, -1473231341);
-      b = ff(b, c, d, a, k[7], 22, -45705983);
-      a = ff(a, b, c, d, k[8], 7, 1770035416);
-      d = ff(d, a, b, c, k[9], 12, -1958414417);
-      c = ff(c, d, a, b, k[10], 17, -42063);
-      b = ff(b, c, d, a, k[11], 22, -1990404162);
-      a = ff(a, b, c, d, k[12], 7, 1804603682);
-      d = ff(d, a, b, c, k[13], 12, -40341101);
-      c = ff(c, d, a, b, k[14], 17, -1502002290);
-      b = ff(b, c, d, a, k[15], 22, 1236535329);
+    a = ff(a, b, c, d, k[0], 7, -680876936);
+    d = ff(d, a, b, c, k[1], 12, -389564586);
+    c = ff(c, d, a, b, k[2], 17, 606105819);
+    b = ff(b, c, d, a, k[3], 22, -1044525330);
+    a = ff(a, b, c, d, k[4], 7, -176418897);
+    d = ff(d, a, b, c, k[5], 12, 1200080426);
+    c = ff(c, d, a, b, k[6], 17, -1473231341);
+    b = ff(b, c, d, a, k[7], 22, -45705983);
+    a = ff(a, b, c, d, k[8], 7, 1770035416);
+    d = ff(d, a, b, c, k[9], 12, -1958414417);
+    c = ff(c, d, a, b, k[10], 17, -42063);
+    b = ff(b, c, d, a, k[11], 22, -1990404162);
+    a = ff(a, b, c, d, k[12], 7, 1804603682);
+    d = ff(d, a, b, c, k[13], 12, -40341101);
+    c = ff(c, d, a, b, k[14], 17, -1502002290);
+    b = ff(b, c, d, a, k[15], 22, 1236535329);
 
-      a = gg(a, b, c, d, k[1], 5, -165796510);
-      d = gg(d, a, b, c, k[6], 9, -1069501632);
-      c = gg(c, d, a, b, k[11], 14, 643717713);
-      b = gg(b, c, d, a, k[0], 20, -373897302);
-      a = gg(a, b, c, d, k[5], 5, -701558691);
-      d = gg(d, a, b, c, k[10], 9, 38016083);
-      c = gg(c, d, a, b, k[15], 14, -660478335);
-      b = gg(b, c, d, a, k[4], 20, -405537848);
-      a = gg(a, b, c, d, k[9], 5, 568446438);
-      d = gg(d, a, b, c, k[14], 9, -1019803690);
-      c = gg(c, d, a, b, k[3], 14, -187363961);
-      b = gg(b, c, d, a, k[8], 20, 1163531501);
-      a = gg(a, b, c, d, k[13], 5, -1444681467);
-      d = gg(d, a, b, c, k[2], 9, -51403784);
-      c = gg(c, d, a, b, k[7], 14, 1735328473);
-      b = gg(b, c, d, a, k[12], 20, -1926607734);
+    a = gg(a, b, c, d, k[1], 5, -165796510);
+    d = gg(d, a, b, c, k[6], 9, -1069501632);
+    c = gg(c, d, a, b, k[11], 14, 643717713);
+    b = gg(b, c, d, a, k[0], 20, -373897302);
+    a = gg(a, b, c, d, k[5], 5, -701558691);
+    d = gg(d, a, b, c, k[10], 9, 38016083);
+    c = gg(c, d, a, b, k[15], 14, -660478335);
+    b = gg(b, c, d, a, k[4], 20, -405537848);
+    a = gg(a, b, c, d, k[9], 5, 568446438);
+    d = gg(d, a, b, c, k[14], 9, -1019803690);
+    c = gg(c, d, a, b, k[3], 14, -187363961);
+    b = gg(b, c, d, a, k[8], 20, 1163531501);
+    a = gg(a, b, c, d, k[13], 5, -1444681467);
+    d = gg(d, a, b, c, k[2], 9, -51403784);
+    c = gg(c, d, a, b, k[7], 14, 1735328473);
+    b = gg(b, c, d, a, k[12], 20, -1926607734);
 
-      a = hh(a, b, c, d, k[5], 4, -378558);
-      d = hh(d, a, b, c, k[8], 11, -2022574463);
-      c = hh(c, d, a, b, k[11], 16, 1839030562);
-      b = hh(b, c, d, a, k[14], 23, -35309556);
-      a = hh(a, b, c, d, k[1], 4, -1530992060);
-      d = hh(d, a, b, c, k[4], 11, 1272893353);
-      c = hh(c, d, a, b, k[7], 16, -155497632);
-      b = hh(b, c, d, a, k[10], 23, -1094730640);
-      a = hh(a, b, c, d, k[13], 4, 681279174);
-      d = hh(d, a, b, c, k[0], 11, -358537222);
-      c = hh(c, d, a, b, k[3], 16, -722521979);
-      b = hh(b, c, d, a, k[6], 23, 76029189);
-      a = hh(a, b, c, d, k[9], 4, -640364487);
-      d = hh(d, a, b, c, k[12], 11, -421815835);
-      c = hh(c, d, a, b, k[15], 16, 530742520);
-      b = hh(b, c, d, a, k[2], 23, -995338651);
+    a = hh(a, b, c, d, k[5], 4, -378558);
+    d = hh(d, a, b, c, k[8], 11, -2022574463);
+    c = hh(c, d, a, b, k[11], 16, 1839030562);
+    b = hh(b, c, d, a, k[14], 23, -35309556);
+    a = hh(a, b, c, d, k[1], 4, -1530992060);
+    d = hh(d, a, b, c, k[4], 11, 1272893353);
+    c = hh(c, d, a, b, k[7], 16, -155497632);
+    b = hh(b, c, d, a, k[10], 23, -1094730640);
+    a = hh(a, b, c, d, k[13], 4, 681279174);
+    d = hh(d, a, b, c, k[0], 11, -358537222);
+    c = hh(c, d, a, b, k[3], 16, -722521979);
+    b = hh(b, c, d, a, k[6], 23, 76029189);
+    a = hh(a, b, c, d, k[9], 4, -640364487);
+    d = hh(d, a, b, c, k[12], 11, -421815835);
+    c = hh(c, d, a, b, k[15], 16, 530742520);
+    b = hh(b, c, d, a, k[2], 23, -995338651);
 
-      a = ii(a, b, c, d, k[0], 6, -198630844);
-      d = ii(d, a, b, c, k[7], 10, 1126891415);
-      c = ii(c, d, a, b, k[14], 15, -1416354905);
-      b = ii(b, c, d, a, k[5], 21, -57434055);
-      a = ii(a, b, c, d, k[12], 6, 1700485571);
-      d = ii(d, a, b, c, k[3], 10, -1894986606);
-      c = ii(c, d, a, b, k[10], 15, -1051523);
-      b = ii(b, c, d, a, k[1], 21, -2054922799);
-      a = ii(a, b, c, d, k[8], 6, 1873313359);
-      d = ii(d, a, b, c, k[15], 10, -30611744);
-      c = ii(c, d, a, b, k[6], 15, -1560198380);
-      b = ii(b, c, d, a, k[13], 21, 1309151649);
-      a = ii(a, b, c, d, k[4], 6, -145523070);
-      d = ii(d, a, b, c, k[11], 10, -1120210379);
-      c = ii(c, d, a, b, k[2], 15, 718787259);
-      b = ii(b, c, d, a, k[9], 21, -343485551);
+    a = ii(a, b, c, d, k[0], 6, -198630844);
+    d = ii(d, a, b, c, k[7], 10, 1126891415);
+    c = ii(c, d, a, b, k[14], 15, -1416354905);
+    b = ii(b, c, d, a, k[5], 21, -57434055);
+    a = ii(a, b, c, d, k[12], 6, 1700485571);
+    d = ii(d, a, b, c, k[3], 10, -1894986606);
+    c = ii(c, d, a, b, k[10], 15, -1051523);
+    b = ii(b, c, d, a, k[1], 21, -2054922799);
+    a = ii(a, b, c, d, k[8], 6, 1873313359);
+    d = ii(d, a, b, c, k[15], 10, -30611744);
+    c = ii(c, d, a, b, k[6], 15, -1560198380);
+    b = ii(b, c, d, a, k[13], 21, 1309151649);
+    a = ii(a, b, c, d, k[4], 6, -145523070);
+    d = ii(d, a, b, c, k[11], 10, -1120210379);
+    c = ii(c, d, a, b, k[2], 15, 718787259);
+    b = ii(b, c, d, a, k[9], 21, -343485551);
 
-      x[0] = add32(a, x[0]);
-      x[1] = add32(b, x[1]);
-      x[2] = add32(c, x[2]);
-      x[3] = add32(d, x[3]);
+    x[0] = add32(a, x[0]);
+    x[1] = add32(b, x[1]);
+    x[2] = add32(c, x[2]);
+    x[3] = add32(d, x[3]);
   }
-
 
   function cmn(q: any, a: any, b: any, x: any, s: any, t: any) {
-      a = add32(add32(a, q), add32(x, t));
-      return add32((a << s) | (a >>> (32 - s)), b);
+    a = add32(add32(a, q), add32(x, t));
+    return add32((a << s) | (a >>> (32 - s)), b);
   }
-
 
   function ff(a: any, b: any, c: any, d: any, x: any, s: any, t: any) {
-      return cmn((b & c) | ((~b) & d), a, b, x, s, t);
+    return cmn((b & c) | (~b & d), a, b, x, s, t);
   }
-
 
   function gg(a: any, b: any, c: any, d: any, x: any, s: any, t: any) {
-      return cmn((b & d) | (c & (~d)), a, b, x, s, t);
+    return cmn((b & d) | (c & ~d), a, b, x, s, t);
   }
-
 
   function hh(a: any, b: any, c: any, d: any, x: any, s: any, t: any) {
-      return cmn(b ^ c ^ d, a, b, x, s, t);
+    return cmn(b ^ c ^ d, a, b, x, s, t);
   }
-
 
   function ii(a: any, b: any, c: any, d: any, x: any, s: any, t: any) {
-      return cmn(c ^ (b | (~d)), a, b, x, s, t);
+    return cmn(c ^ (b | ~d), a, b, x, s, t);
   }
 
-
   function md51(s: any) {
-      let n     = s.length,
-          state = [1732584193, -271733879, -1732584194, 271733878],
-          i;
+    let n = s.length,
+      state = [1732584193, -271733879, -1732584194, 271733878],
+      i;
 
-      for (i = 64; i <= s.length; i += 64) {
-          md5cycle(state, md5blk(s.substring(i - 64, i)));
-      }
-      s = s.substring(i - 64);
-      let tail = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-      for (i = 0; i < s.length; i++) {
-          tail[i >> 2] |= s.charCodeAt(i) << ((i % 4) << 3);
-      }
-      tail[i >> 2] |= 0x80 << ((i % 4) << 3);
-      if (i > 55) {
-          md5cycle(state, tail);
-          for (i = 0; i < 16; i++) {
-              tail[i] = 0;
-          }
-      }
-      tail[14] = n * 8;
+    for (i = 64; i <= s.length; i += 64) {
+      md5cycle(state, md5blk(s.substring(i - 64, i)));
+    }
+    s = s.substring(i - 64);
+    let tail = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (i = 0; i < s.length; i++) {
+      tail[i >> 2] |= s.charCodeAt(i) << (i % 4 << 3);
+    }
+    tail[i >> 2] |= 0x80 << (i % 4 << 3);
+    if (i > 55) {
       md5cycle(state, tail);
+      for (i = 0; i < 16; i++) {
+        tail[i] = 0;
+      }
+    }
+    tail[14] = n * 8;
+    md5cycle(state, tail);
 
-      return state;
+    return state;
   }
 
   /* there needs to be support for Unicode here,
@@ -648,51 +626,50 @@ export const md5 = (function (global) {
    * providing access to strings as preformed UTF-8
    * 8-bit unsigned value arrays.
    */
-  function md5blk(s: any) { /* I figured global was faster.   */
-      let md5blks = [],
-          i; /* Andy King said do it this way. */
+  function md5blk(s: any) {
+    /* I figured global was faster.   */
+    let md5blks = [],
+      i; /* Andy King said do it this way. */
 
-      for (i = 0; i < 64; i += 4) {
-          md5blks[i >> 2] = s.charCodeAt(i) + (s.charCodeAt(i + 1) << 8) + (s.charCodeAt(i + 2) << 16) + (s.charCodeAt(i + 3) << 24);
-      }
+    for (i = 0; i < 64; i += 4) {
+      md5blks[i >> 2] =
+        s.charCodeAt(i) + (s.charCodeAt(i + 1) << 8) + (s.charCodeAt(i + 2) << 16) + (s.charCodeAt(i + 3) << 24);
+    }
 
-      return md5blks;
+    return md5blks;
   }
-
 
   function rhex(n: any) {
-      let s = '',
-          j = 0;
+    let s = '',
+      j = 0;
 
-      for (; j < 4; j++) {
-          s += hex_chr[(n >> (j * 8 + 4)) & 0x0F] + hex_chr[(n >> (j * 8)) & 0x0F];
-      }
+    for (; j < 4; j++) {
+      s += hex_chr[(n >> (j * 8 + 4)) & 0x0f] + hex_chr[(n >> (j * 8)) & 0x0f];
+    }
 
-      return s;
+    return s;
   }
-
 
   function hex(x: any) {
-      for (let i = 0; i < x.length; i++) {
-          x[i] = rhex(x[i]);
-      }
+    for (let i = 0; i < x.length; i++) {
+      x[i] = rhex(x[i]);
+    }
 
-      return x.join('');
+    return x.join('');
   }
-
 
   /* the standard function is much faster, so if possible, use it. */
   function add32(a: any, b: any) {
-      return (use_alternate_add32 ? alternate : standard)();
+    return (use_alternate_add32 ? alternate : standard)();
 
-      function standard() {
-          return (a + b) & 0xFFFFFFFF;
-      }
+    function standard() {
+      return (a + b) & 0xffffffff;
+    }
 
-      function alternate() {
-          let lsw = (a & 0xFFFF) + (b & 0xFFFF),
-              msw = (a >> 16) + (b >> 16) + (lsw >> 16);
-          return (msw << 16) | (lsw & 0xFFFF);
-      }
+    function alternate() {
+      let lsw = (a & 0xffff) + (b & 0xffff),
+        msw = (a >> 16) + (b >> 16) + (lsw >> 16);
+      return (msw << 16) | (lsw & 0xffff);
+    }
   }
 })(window);
